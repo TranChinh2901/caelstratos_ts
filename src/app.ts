1d965da50395dab50aec81c416f97b5d3315097a
Tranchinh2901 <chinh99078@donga.edu.vn>
2025-07-07 22:10:44 +0700

    remove node_modules from repo

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,6 @@ getConnection()
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  // console.log(`Database connection established successfully. ${getConnection}`);
+  
   
 });
